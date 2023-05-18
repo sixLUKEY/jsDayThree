8c2ea86b2f22178a48227ca3fa924d91996f6740
sixLUKEY <marcoslukerojo@gmail.com>
2023-05-18 16:02:42 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// let street = 'Grove Street';
+// console.log(typeof street);
+
+let street = 5
+console.log(typeof street);
+
+let name = 'Jeff';
+console.log(typeof name);
+
+let gymBro = false;
+console.log(typeof gymBro);
+
+let guests = [];
+console.log(typeof guests);
+
+let nothing = undefined;
+console.log(typeof nothing);
+
+let colors = null;
+console.log(typeof colors);
+
---
+++ src/app2.js
@@ -0,0 +1,18 @@
+let person1 = {
+    name: 'John',
+        age: 20,
+}
+
+let person2 = {
+    name: 'Jane',
+        age: 21,
+}
+
+console.log(person1, person2);
+
+// let people = [];
+// people.push(person1);
+// people.push(person2);
+
+// console.log(people);
+
